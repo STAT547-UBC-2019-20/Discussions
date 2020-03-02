1bd4e6053ea97092af8d4793bed4e4b89f774a00
Firas Moosvi <firas@moosvi.com>
2020-03-02 15:42:31 -0800

    Add files via upload

---
+++ test_script.R
@@ -0,0 +1,7 @@
+## This is a test R script to make sure your computers are able to execute Rscript files
+
+library(tidyverse)
+
+print(sessionInfo())
+
+print('Congratulations! It seems that you are able to run R scripts using a terminal!')
