3f09a4a9b895ab14abf5f89f2789af2e4e59ae45
Firas Moosvi <firas@moosvi.com>
2020-03-17 10:30:12 -0700

    cm107 participation files

---
+++ participation/cm107/app.R
@@ -0,0 +1,17 @@
+## YOUR SOLUTION HERE
+
+# author: YOUR NAME
+# date: 2020-03-17
+
+"This script is the main file that creates a Dash app.
+
+Usage: app.R
+"
+
+# 1. Load libraries
+
+# 2. Create Dash instance
+
+# 3. Specify App layout
+
+# 4. Run app
